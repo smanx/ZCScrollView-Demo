401cb31f074b20f17d864e96e5c332ba1ddfe2ef
smanx <QF@mac-mini.local>
2016-09-23 19:38:57 +0800

    Initial Commit

---
+++ Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project
+//
+//  Created by QF on 2016/9/23.
+//  Copyright © 2016年 zc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
